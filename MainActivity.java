097c0cf67f470e5fa7971437d7e60f1fd01171ca
bcburger <bcburger@loyola.edu>
2019-09-24 12:26:09 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,13 @@
-//Main Activity
+package com.example.brocburger.blackjack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
